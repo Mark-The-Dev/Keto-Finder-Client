df3a43179104ae600ea9327f7464954482e79c68
Mark <SwiftTactics@gmail.com>
2021-11-14 20:13:09 -0500

    added new restaurant service methods

--- src/app/service/restaurant.service.ts
+++ src/app/service/restaurant.service.ts
@@ -1,7 +1,7 @@
 import {Inject, Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {RestaurantBundleDTO} from '../models/RestaurantBundleDTO';
+import {RestaurantBundleDTO, RestaurantItem} from '../models/RestaurantBundleDTO';
 
 @Injectable({providedIn: 'root'})
 export class RestaurantService {
@@ -14,4 +14,18 @@ export class RestaurantService {
   getAllMeals(): Observable<RestaurantBundleDTO[]> {
     return this.http.get<RestaurantBundleDTO[]>(`${this.env.apiUrl}/restaurants`);
   }
+
+  getRestaurantByName(restaurantName: string): Observable<RestaurantBundleDTO> {
+    return this.http.get<RestaurantBundleDTO>(`${this.env.apiUrl}/restaurants/${restaurantName}`);
+  }
+
+  addRestaurant(restaurant: RestaurantBundleDTO): void {
+    this.http.post<RestaurantBundleDTO>(`${this.env.apiUrl}/restaurants`, restaurant);
+  }
+
+  addRestaurantMeal(restaurantMeal: RestaurantItem, restaurantId: string): void {
+    this.http.post<RestaurantBundleDTO>(`${this.env.apiUrl}/restaurants/${restaurantId}`, restaurantMeal);
+  }
+
+
 }
