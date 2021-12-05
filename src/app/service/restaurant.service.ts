8c031cfccca2ea8b760ebb9313449070d42b3eda
Mark <SwiftTactics@gmail.com>
2021-12-05 18:28:20 -0500

    added mat form fields

--- src/app/service/restaurant.service.ts
+++ src/app/service/restaurant.service.ts
@@ -13,11 +13,11 @@ export class RestaurantService {
   constructor(private http: HttpClient, @Inject('env') private env) {}
 
   getAllMeals(): Observable<RestaurantBundleDTO[]> {
-    return this.http.get<RestaurantBundleDTO[]>(`${this.env.apiUrl}/restaurants`);
+    return this.http.get<RestaurantBundleDTO[]>(`${this.env.apiUrl}/restaurants`, this.httpOptions);
   }
 
   getRestaurantById(restaurantId: string): Observable<RestaurantBundleDTO> {
-    return this.http.get<RestaurantBundleDTO>(`${this.env.apiUrl}/restaurants/${restaurantId}`);
+    return this.http.get<RestaurantBundleDTO>(`${this.env.apiUrl}/restaurants/${restaurantId}`, this.httpOptions);
   }
 
   addRestaurant(restaurant: RestaurantBundleDTO): void {
@@ -25,7 +25,7 @@ export class RestaurantService {
   }
 
   addRestaurantMeal(restaurantMeal: RestaurantItem, restaurantId: string): void {
-    this.http.post<RestaurantItem>(`${this.env.apiUrl}/restaurants/addMeal/${restaurantId}`, restaurantMeal)
+    this.http.post<RestaurantItem>(`${this.env.apiUrl}/restaurants/addMeal/${restaurantId}`, restaurantMeal, this.httpOptions)
       .pipe(take(1)).subscribe();
   }
 
