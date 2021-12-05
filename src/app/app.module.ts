a98d91027aafe9d54120560efc00e348eddd7494
Mark <SwiftTactics@gmail.com>
2021-12-05 17:40:42 -0500

    added form styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatOptionModule} from '@angular/material/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import { AddMealComponent } from './add-meal/add-meal.component';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { AddMealComponent } from './add-meal/add-meal.component';
     MatSelectModule,
     MatOptionModule,
     ReactiveFormsModule,
+    MatInputModule,
   ],
   providers: [
     { provide: 'env', useValue: environment },
