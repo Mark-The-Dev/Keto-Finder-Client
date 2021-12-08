2c2c0667f8219033b5691c9539cf80f0081d12a2
Mark <SwiftTactics@gmail.com>
2021-12-08 18:55:00 -0500

    added material style buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,26 +2,31 @@ import {Component, OnInit} from '@angular/core';
 import {RestaurantService} from '../service/restaurant.service';
 import {Observable} from 'rxjs';
 import {RestaurantBundleDTO} from '../models/RestaurantBundleDTO';
-import {NavigationExtras, Router} from '@angular/router';
+import { Router} from '@angular/router';
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: 'app-home', // component name in template.
+  templateUrl: './home.component.html', // template file
+  styleUrls: ['./home.component.scss'] // style sheet
 })
 export class HomeComponent implements OnInit{
 
   constructor(
-    public restaurantService: RestaurantService,
-    private router: Router
+    public restaurantService: RestaurantService, // Injected service.
+    private router: Router // Injected Router
   ) {}
 
+  // Observable with restaurant data.
   public restaurantResponse$: Observable<RestaurantBundleDTO[]>;
 
+  // Lifecycle hook - OnInit will fire when the component is created
   ngOnInit(): void {
+    // Request to grab all restaurant data.
     this.restaurantResponse$ = this.restaurantService.getAllMeals();
   }
+
+  // Simple navigate method for the html template
   navigateTo(restaurantName: string, restaurantInfo: RestaurantBundleDTO): void{
     const urlSlug = restaurantName.split(' ').join('-');
     this.router.navigateByUrl(`/restaurants/${urlSlug}`, {state : {restaurant: restaurantInfo}});
